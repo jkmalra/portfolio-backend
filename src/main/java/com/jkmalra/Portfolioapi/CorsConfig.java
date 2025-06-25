731a0af004f1ed47f299951fc4468a1e6d57968c
Jaskaran Singh <jaskaranmain@gmail.com>
2025-06-25 14:36:35 +0530

    Update CORS- for live included

--- src/main/java/com/jkmalra/Portfolioapi/CorsConfig.java
+++ src/main/java/com/jkmalra/Portfolioapi/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+                        .allowedOrigins("http://127.0.0.1:5500", //for local
+                                        "https://jkmalra.github.io") //for github.io
                         .allowedMethods("GET", "POST")
                         .allowedHeaders("*");
             }
